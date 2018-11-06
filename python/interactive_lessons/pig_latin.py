1d1d75518ee5784d41cc056093975c8782c6e0d9
Lillian N <ngo.lilli94@gmail.com>
2018-11-05 19:07:43 -0500

    Pig Latin Translator exercise

---
+++ python/interactive_lessons/pig_latin.py
@@ -0,0 +1,21 @@
+print "Welcome to the Pig Latin Translator!"
+
+# raw_input takes in user input
+original = raw_input("Please enter a word: ")
+
+# Create variable for suffix
+pyg = "ay" 
+
+# If statement to ensure user input a non-numeric word
+if len(original) > 0 and original.isalpha():
+    #Convert input to lower case
+    word = original.lower()
+    #Variable to hold first letter of word
+    first = word[0]
+    #Concatenating word + first + pyg together for translation into new variable
+    new_word = word + first + pyg
+    #Returning new word without repeat of first letter in original & new position by slicing
+    new_word = new_word[1:len(new_word)]
+    print original + " in Pig Latin is " + new_word
+else:
+    print "Your input is invalid :("
