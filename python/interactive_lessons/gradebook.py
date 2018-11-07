48170d5796af1bf6a8cbb10cb69bafe347cd4217
Lillian N <ngo.lilli94@gmail.com>
2018-11-07 14:19:27 -0500

    Gradebook project created

---
+++ python/interactive_lessons/gradebook.py
@@ -0,0 +1,20 @@
+kashima = {
+    "name": "Ryuichi",
+    "homework": [],
+    "quizzes": [],
+    "tests": []
+}
+
+kamitani = {
+    "name": "Hayato",
+    "homework": [],
+    "quizzes": [],
+    "tests": []
+}
+
+inomata = {
+    "name": "Maria",
+    "homework": [],
+    "quizzes": [],
+    "tests": []
+}
