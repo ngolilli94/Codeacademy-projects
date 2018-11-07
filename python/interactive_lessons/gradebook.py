d190a0c21baa9aca0514865905a3b6f761202301
Lillian N <ngo.lilli94@gmail.com>
2018-11-07 14:34:38 -0500

    Basic info & functions added

--- python/interactive_lessons/gradebook.py
+++ python/interactive_lessons/gradebook.py
@@ -1,20 +1,38 @@
+# Python Lesson 6 - Challenge Course
+
 kashima = {
-    "name": "Ryuichi",
-    "homework": [],
-    "quizzes": [],
-    "tests": []
+    "name": "Ryuuichi",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
 }
 
 kamitani = {
     "name": "Hayato",
-    "homework": [],
-    "quizzes": [],
-    "tests": []
+    "homework": [90.0, 87.0, 75.0, 88.0],
+    "quizzes": [90.0, 75.0, 88.0],
+    "tests": [99.0, 97.0]
 }
 
 inomata = {
     "name": "Maria",
-    "homework": [],
-    "quizzes": [],
-    "tests": []
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [95.0, 98.0, 96.0],
+    "tests": [100.0, 100.0]
 }
+
+# Student Roster
+students = [kashima, kamitani, inomata]
+
+for student in students:
+    print student["name"]
+    print student["homework"]
+    print student["quizzes"]
+    print student["tests"]
+    print
+
+# Using built-in sum(), float(), & len() functions to obtain class avg
+def average(numbers):
+    total = sum(numbers)
+    total = float(total)
+    return total / len(numbers)
