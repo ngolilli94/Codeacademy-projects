333a66928f5f3e7d566889d7d484715a28b15dbd
Lillian N <ngo.lilli94@gmail.com>
2018-11-14 13:16:33 -0500

    Exam statistics mini project created

---
+++ python/interactive_lessons/exam_statistics.py
@@ -0,0 +1,4 @@
+# Mini project for practicing functions, lists, & translating math formula into programming statements
+
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+print "Grades:", grades
